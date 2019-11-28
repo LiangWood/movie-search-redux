90d7f4ae4eae8d9e91531ff8b0e99a6718735f08
Jack Liang <jack.liang.tw@pmgi.local>
2019-11-28 15:46:34 +0800

    Updates

---
+++ precache-manifest.37216e46d530befdf678296f92cce0d9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e39e5bf02b94d82c9df05ed9ffc4796",
+    "url": "/movie-search-redux/index.html"
+  },
+  {
+    "revision": "17c6e8ca5f6308ae80c7",
+    "url": "/movie-search-redux/static/css/main.2bf8a911.chunk.css"
+  },
+  {
+    "revision": "f13008077723e9f9683d",
+    "url": "/movie-search-redux/static/js/2.b578ca23.chunk.js"
+  },
+  {
+    "revision": "17c6e8ca5f6308ae80c7",
+    "url": "/movie-search-redux/static/js/main.bff31583.chunk.js"
+  },
+  {
+    "revision": "e105b6fd91e0298e72fa",
+    "url": "/movie-search-redux/static/js/runtime-main.8dd34036.js"
+  }
+]);
